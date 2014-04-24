4e62fbcbb2b380d75b03d845c8b89cda1d3c0411
Jesse Nelson <spheromak@gmail.com>
2014-04-23 23:13:27 -0700

    add a file that drone can run for the whole test setup

---
+++ test/support/ci.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+test/support/chef_config.sh
+go get -v github.com/ctdk/goiardi
+test/support/start_server.sh
+go get code.google.com/p/go.tools/cmd/vet
+go install code.google.com/p/go.tools/cmd/vet
+go vet
+go test -v -cover
+test/support/stop_server.sh
